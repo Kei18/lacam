ca29317899ca6929155a5bcd1dc7dc793e5e63cd
Zhenghong Yu <zyu379@wisc.edu>
2024-01-30 02:38:38 +0800

    fix timer

--- main.cpp
+++ main.cpp
@@ -77,11 +77,11 @@ int main(int argc, char* argv[])
   for (int i = 0; i < ngoals; i += nagents_with_new_goals) {
     // info output
     auto current_time = std::chrono::steady_clock::now();
-    auto elapsed_time = std::chrono::duration_cast<std::chrono::seconds>(current_time - timer).count();
+    auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - timer).count();
 
-    if (!debug && elapsed_time >= 1 && cache_access > 0) {
+    if (!debug && elapsed_time >= 1000 && cache_access > 0) {
       double cacheRate = static_cast<double>(cache_hit) / cache_access * 100.0;
-      console->info("Elapsed Time: 1 second   |   Goals Reached: {:5}   |   Cache Rate: {:.2f}%    |   Steps Used: {:5}", i, cacheRate, step);
+      console->info("Elapsed Time: {:5}ms   |   Goals Reached: {:5}   |   Cache Rate: {:.2f}%    |   Steps Used: {:5}", elapsed_time, i, cacheRate, step);
       // Reset the timer
       timer = std::chrono::steady_clock::now();
     }
