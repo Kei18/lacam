955d5f479b4a09069b89860c6a141742a27dba0a
Zhenghong Yu <zyu379@wisc.edu>
2024-02-05 15:56:24 +0800

    add experiment

--- main.cpp
+++ main.cpp
@@ -9,39 +9,18 @@ int main(int argc, char* argv[])
 
   // arguments parser
   argparse::ArgumentParser program("lacam", "0.1.0");
-  program.add_argument("-m", "--map").help("map file").required();  // map file
-  program.add_argument("-ng", "--ngoals")
-    .help("number of goals")
-    .required();  // number of goals: agent first go to get goal, and then
-  // return to unloading port
-  program.add_argument("-gk", "--goals-k")
-    .help("maximum k different number of goals in m segment of all goals")
-    .required();
-  program.add_argument("-gm", "--goals-m")
-    .help("maximum k different number of goals in m segment of all goals")
-    .required();
-  program.add_argument("-na", "--nagents")
-    .help("number of agents")
-    .required();  // number of agents
-  program.add_argument("-s", "--seed")
-    .help("seed")
-    .default_value(std::string("0"));  // random seed
-  program.add_argument("-v", "--verbose")
-    .help("verbose")
-    .default_value(std::string("0"));  // verbose
-  program.add_argument("-t", "--time_limit_sec")
-    .help("time limit sec")
-    .default_value(std::string("10"));  // time limit (second)
-  program.add_argument("-o", "--output")
-    .help("output file")
-    .default_value(std::string("./result/result.txt"));  // output file
-  program.add_argument("-l", "--log_short")
-    .default_value(false)
-    .implicit_value(true);
-  program.add_argument("-d", "--debug")
-    .help("enable debug logging")
-    .default_value(false)
-    .implicit_value(true);  // debug mode
+  program.add_argument("-m", "--map").help("map file").required();                                                                              // map file
+  program.add_argument("-ng", "--ngoals").help("number of goals").required();                                                                   // number of goals: agent first go to get goal, and then return to unloading port
+  program.add_argument("-gk", "--goals-k").help("maximum k different number of goals in m segment of all goals").required();
+  program.add_argument("-gm", "--goals-m").help("maximum k different number of goals in m segment of all goals").required();
+  program.add_argument("-na", "--nagents").help("number of agents").required();                                                                 // number of agents
+  program.add_argument("-s", "--seed").help("seed").default_value(std::string("0"));                                                            // random seed
+  program.add_argument("-v", "--verbose").help("verbose").default_value(std::string("0"));                                                      // verbose
+  program.add_argument("-t", "--time_limit_sec").help("time limit sec").default_value(std::string("10"));                                       // time limit (second)
+  program.add_argument("-o", "--output_step_result").help("step result output file").default_value(std::string("./result/step_result.txt"));    // output file
+  program.add_argument("-c", "--output_csv_result").help("csv output file").default_value(std::string("./result/result.csv"));
+  program.add_argument("-l", "--log_short").default_value(false).implicit_value(true);
+  program.add_argument("-d", "--debug").help("enable debug logging").default_value(false).implicit_value(true);                                 // debug mode
 
   try {
     program.parse_known_args(argc, argv);
@@ -54,13 +33,13 @@ int main(int argc, char* argv[])
 
   // setup instance
   const auto verbose = std::stoi(program.get<std::string>("verbose"));
-  const auto time_limit_sec =
-    std::stoi(program.get<std::string>("time_limit_sec"));
+  const auto time_limit_sec = std::stoi(program.get<std::string>("time_limit_sec"));
   auto deadline = Deadline(time_limit_sec * 1000);
   const auto seed = std::stoi(program.get<std::string>("seed"));
   auto MT = std::mt19937(seed);
   const auto map_name = program.get<std::string>("map");
-  const auto output_name = program.get<std::string>("output");
+  const auto output_step_name = program.get<std::string>("output_step_result");
+  const auto output_csv_name = program.get<std::string>("output_csv_result");
   const auto log_short = program.get<bool>("log_short");
   const auto ngoals = std::stoi(program.get<std::string>("ngoals"));
   const auto goals_m = std::stoi(program.get<std::string>("goals-m"));
@@ -82,7 +61,7 @@ int main(int argc, char* argv[])
   console->info("Seed:             {}", seed);
   console->info("Verbose:          {}", verbose);
   console->info("Time limit (sec): {}", time_limit_sec);
-  console->info("Output file:      {}", output_name);
+  console->info("Output file:      {}", output_step_name);
   console->info("Log short:        {}", log_short);
   console->info("Debug:            {}", debug);
 
@@ -159,7 +138,7 @@ int main(int argc, char* argv[])
 
     // post processing
     log.print_stats(verbose, ins, comp_time_ms);
-    log.make_step_log(ins, output_name, comp_time_ms, map_name, seed,
+    log.make_step_log(ins, output_step_name, comp_time_ms, map_name, seed,
       log_short);
 
     // assign new goals
@@ -171,6 +150,16 @@ int main(int argc, char* argv[])
   double total_cache_rate = static_cast<double>(cache_hit) / cache_access * 100.0;
   console->info("Total Goals Reached: {:5}   |   Total Cache Hit Rate: {:2.2f}%    |   Total Steps Used: {:5}", ngoals, total_cache_rate, step);
   log.make_life_long_log(ins, seed);
+
+  std::ofstream file(output_csv_name, std::ios::app);
+
+  if (!file.is_open()) {
+    std::cerr << "Failed to open file: " << output_csv_name << std::endl;
+    return 1;
+  }
+
+  file << map_name << "," << ngoals << "," << nagents << "," << seed << "," << verbose << "," << time_limit_sec << "," << goals_m << "," << goals_k << std::endl;
+  file.close();
 
   return 0;
 }
